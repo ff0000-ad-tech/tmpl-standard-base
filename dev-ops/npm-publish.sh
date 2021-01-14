7dc953aab563c894cda0daa8f6257f9c93469511
Greg Connell <gconnell@160over90.com>
2021-01-13 16:32:06 -0800

    updates published note

--- dev-ops/npm-publish.sh
+++ dev-ops/npm-publish.sh
@@ -5,3 +5,17 @@ node set-package-name.js --package ../package.json
 echo Publishing to NPM
 cd ..
 np --any-branch --no-release-draft
+
+# get release version
+PKG_NAME=`node -pe "require('../package.json').name"`
+RELEASE_VERSION=`node -pe "require('../package.json').version"`
+
+# note
+echo
+echo "Done.\033[1;31m Be sure to update BSA's version reference! \033[0m"
+echo " https://github.com/ff0000-tech/build-source-assembler/blob/master/package.json"
+echo "  \"buildSources\": {"
+echo "    ..."
+echo "    \033[1;32m\"$PKG_NAME\": \"$RELEASE_VERSION\" \033[0m"
+echo "    ..."
+echo "  }"
