9614737e8545ba9d69f3ea26a9196204b36c2dad
Greg Connell <gconnell@160over90.com>
2021-01-20 23:24:55 -0800

    test install working fine

--- 1-build/300x250/.inline-imports.js
+++ 1-build/300x250/.inline-imports.js
@@ -1,5 +1,5 @@
 import { InlineSrc } from '@ff0000-ad-tech/ad-assets'
 window.InlineSrc = InlineSrc
 
-import preloaderWhite from './images/preloader_white.gif?inline=true'
-InlineSrc.add('./images/preloader_white.gif', preloaderWhite)
+import dpPreloader from '../common/images/dp-preloader.gif?inline=true'
+InlineSrc.add('../common/images/dp-preloader.gif', dpPreloader)
