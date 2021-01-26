a2c107e107c8894c8ee285a1fbe772922b6db600
Greg Connell <gconnell@160over90.com>
2021-01-25 18:31:37 -0800

    updates necessary for remotable compiles to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,24 @@
+/**
+ * NOTE:
+ * 		This is a proxy to:
+ * 		@ff0000-ad-tech/wp-deploy-manager/webpack-config.js
+ *
+ * This provides visibility & flexibility as to where the full webpack-config.js lives,
+ * whether installed as a template dependency or running externally.
+ *
+ * A similar one lives at the top of the @ff0000-ad-tech/wp-deploy-manager.
+ */
 const wp = require('./node_modules/@ff0000-ad-tech/wp-deploy-manager/webpack.config.js')
+// !!!
+// TODO #remotable:
+/*
+			const wp = require('./node_modules/@ff0000-ad-tech/wp-deploy-manager/webpack-config.js')
 
-module.exports = config => {
+			module.exports = (config) => {
+				return wp.execute(config)
+			}
+*/
+// !!!
+module.exports = (config) => {
 	return wp.execute(config, __dirname)
 }
