05ca7cf4968ea0f847b616febaa210f8fa313f05
Joe Capra <jcapra@160over90.com>
2021-01-27 10:58:14 -0800

    initial commit

--- 1-build/300x250/components/Ad/index.jsx
+++ 1-build/300x250/components/Ad/index.jsx
@@ -1,19 +1,10 @@
 import { h, render, Component, createRef } from 'preact'
 import { ImageManager } from '@ff0000-ad-tech/ad-control'
 
-import BrandLogo from '../BrandLogo'
-import CanvasElement from '../CanvasElement'
-import CanvasElementImage from '../CanvasElementImage'
-import CanvasElementMask from '../CanvasElementMask'
-import CanvasElementText from '../CanvasElementText'
-import CanvasElementTween from '../CanvasElementTween'
 import TextFit from '../TextFit'
-import SplitText from '../SplitText'
 
 import './styles.scss'
 import '@common/fonts/template_font.woff'
-import '@size/images/160over90-logo.png'
-import '@size/images/160over90-logo-small.png'
 
 class Ad extends Component {
 	constructor(props) {
@@ -27,22 +18,9 @@ class Ad extends Component {
 	render() {
 		return (
 			<div className="a">
-				{/* <TextFit minFontSize={1} maxFontSize={50} className="tf__box">
+				<TextFit minFontSize={1} maxFontSize={50} className="tf__box">
 					The quick brown fox jumps over the lazy dog. This is some more good stuff sdfg sdfgsd dsfg sfgh dgdfh fdgh
-				</TextFit> */}
-				{/* <CanvasElement ref={(el) => (this.canvas = el)} className="a__canvas" width={adParams.adWidth} height={adParams.adHeight} /> */}
-				{/* <CanvasElementTween ref={(el) => (this.canvas = el)} className="a__canvas" width={adParams.adWidth} height={adParams.adHeight} /> */}
-				{/* <CanvasElementImage ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} /> */}
-				{/* <CanvasElementMask ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} /> */}
-				{/* <CanvasElementText ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} /> */}
-				{/* <SplitText>This is some cool text</SplitText> */}
-				<BrandLogo ref={(el) => (this.brandlogo = el)} />
-				<div ref={(el) => (this.footer = el)} className="a__footer">
-					<img ref={(el) => (this.logo = el)} className="a__logo" src={this.getImage('160over90-logo-small')} />
-					<div ref={(el) => (this.cta = el)} className="a__cta">
-						LEARN MORE
-					</div>
-				</div>
+				</TextFit>
 			</div>
 		)
 	}
