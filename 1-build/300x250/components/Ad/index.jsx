d40a0de38aeaa12e3e8042de1a878d54daa688c4
Joe Capra <jcapra@160over90.com>
2021-01-27 16:05:09 -0800

    Remove unneeded images

--- 1-build/300x250/components/Ad/index.jsx
+++ 1-build/300x250/components/Ad/index.jsx
@@ -1,11 +1,7 @@
 import { h, render, Component, createRef } from 'preact'
 import { ImageManager } from '@ff0000-ad-tech/ad-control'
 
-import CanvasElement from '../CanvasElement'
-import CanvasElementImage from '../CanvasElementImage'
-import CanvasElementMask from '../CanvasElementMask'
-import CanvasElementText from '../CanvasElementText'
-import CanvasElementTween from '../CanvasElementTween'
+import SplitText from '../SplitText'
 
 import './styles.scss'
 import '@common/fonts/template_font.woff'
@@ -13,71 +9,12 @@ import '@common/fonts/template_font.woff'
 class Ad extends Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			activeItem: 'element',
-		}
-	}
-
-	getImage = (name) => {
-		return ImageManager.get(name).src
 	}
 
 	render() {
 		return (
 			<div className="a">
-				{this.state.activeItem == 'element' ? (
-					<CanvasElement ref={(el) => (this.canvas = el)} className="a__canvas" width={adParams.adWidth} height={adParams.adHeight} />
-				) : null}
-				{this.state.activeItem == 'tween' ? (
-					<CanvasElementTween ref={(el) => (this.canvas = el)} className="a__canvas" width={adParams.adWidth} height={adParams.adHeight} />
-				) : null}
-				{this.state.activeItem == 'image' ? (
-					<CanvasElementImage ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} />
-				) : null}
-				{this.state.activeItem == 'mask' ? (
-					<CanvasElementMask ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} />
-				) : null}
-				{this.state.activeItem == 'text' ? (
-					<CanvasElementText ref={(el) => (this.canvas = el)} width={adParams.adWidth} height={adParams.adHeight} />
-				) : null}
-
-				<div className="a__nav">
-					<button
-						onClick={() => {
-							this.setState({ activeItem: 'element' })
-						}}
-					>
-						Element
-					</button>
-					<button
-						onClick={() => {
-							this.setState({ activeItem: 'image' })
-						}}
-					>
-						Image
-					</button>
-					<button
-						onClick={() => {
-							this.setState({ activeItem: 'mask' })
-						}}
-					>
-						Mask
-					</button>
-					<button
-						onClick={() => {
-							this.setState({ activeItem: 'text' })
-						}}
-					>
-						Text
-					</button>
-					<button
-						onClick={() => {
-							this.setState({ activeItem: 'tween' })
-						}}
-					>
-						Tween
-					</button>
-				</div>
+				<SplitText>This is some cool text</SplitText>
 			</div>
 		)
 	}
