6630592e7d3b53eada494b20e5207bc432e2f627
Greg Connell <gconnell@160over90.com>
2021-05-19 16:00:50 -0700

    hammering in dps connectivity

--- 1-build/common/js/Preflight.js
+++ 1-build/common/js/Preflight.js
@@ -1,5 +1,6 @@
 import AdData from '@common/js/AdData.js'
 import { ImageManager } from '@ff0000-ad-tech/ad-control'
+import dps from '@ff0000-ad-tech/ad-dps'
 /*-- Red.Imports.head.start --*/
 /*-- Red.Imports.head.end --*/
 
@@ -12,65 +13,55 @@ import { ImageManager } from '@ff0000-ad-tech/ad-control'
 	Once resolved, control moves to AdData.
 */
 export class Preflight {
-	static init() {
+	static init = async () => {
 		console.log('Preflight.init()')
-		return new Promise((resolve, reject) => {
-			let promises = [
-				// this.loadDynamicJS('define-your-case-id')
-			]
-
-			Promise.all(promises)
-				.then(() => {
-					this.addPreloadedImages()
-					this.prepareAdData()
-				})
-				.then(() => {
-					resolve()
-				})
-				.catch((err) => {
-					reject(err)
-				})
-		})
-	}
-
-	static addPreloadedImages() {
-		console.log('Preflight.addPreloadedImages()')
-		ImageManager.addToDictionary(assets.preloadedImages)
-	}
-
-	static prepareAdData() {
-		console.log('Preflight.prepareAdData()')
-
-		global.adData = new AdData()
-	}
-
-	/**
-		Method for loading dynamic, compiled ES6 modules at runtime. This should be threaded into 
-		Preflight.init()'s promise chain, as needed.
-
-		You must:
-			- define THIS_CASE__ID
-			- replace THIS_CASE__ASSET_PATH with a string
-			- handle the implementation of the loaded module.
-	*/
-	static loadDynamicJS(id) {
-		return new Promise((resolve, reject) => {
-			switch (id) {
-				/*
-				case THIS_CASE__ID: // ex: '300x250_Endframe'
-					import('THIS_CASE__ASSET_PATH') // ex: '@common/dynamic_js/300x250_Endframe.js'
-						.then(module => {
-							// example implementation of loaded ES6 module
-							global[module.default.name] = module.default
-							resolve()
-						})
-						.catch(err => reject(err))
-					break
-				*/
-				default:
-					console.log(`Common.loadDynamicJS() has no import case for: ${id}`)
-					resolve()
-			}
-		})
+		await Promise.all([
+			// loadDynamicJS('define-your-case-id')
+			loadDpsData()
+		])
+		addPreloadedImages()
+		prepareAdData()
 	}
 }
+
+const loadDpsData = async () => {
+	console.log('Preflight.loadDpsData()')
+	const data = await dps.loadFeed(adParams.dps)
+	console.log(data)
+}
+
+const addPreloadedImages = () => {
+	console.log('Preflight.addPreloadedImages()')
+	ImageManager.addToDictionary(assets.preloadedImages)
+}
+
+const prepareAdData = () => {
+	console.log('Preflight.prepareAdData()')
+
+	global.adData = new AdData()
+}
+
+/**
+	Method for loading dynamic, compiled ES6 modules at runtime. This should be threaded into 
+	Preflight.init()'s promise chain, as needed.
+
+	You must:
+		- define THIS_CASE__ID
+		- replace THIS_CASE__ASSET_PATH with a string
+		- handle the implementation of the loaded module.
+*/
+const loadDynamicJS = async (id) => {
+	switch (id) {
+		/*
+		case THIS_CASE__ID: // ex: '300x250_Endframe'
+			await import('THIS_CASE__ASSET_PATH') // ex: '@common/dynamic_js/300x250_Endframe.js'
+			// example implementation of loaded ES6 module
+			global[module.default.name] = module.default
+			break
+		*/
+		default:
+			console.log(`Common.loadDynamicJS() has no import case for: ${id}`)
+
+	}
+}
+
