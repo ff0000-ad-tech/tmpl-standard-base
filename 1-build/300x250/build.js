0a3f9ac261d69538d1eb51e986157270265432e2
Greg Connell <gconnell@160over90.com>
2021-06-11 16:55:14 -0700

    preparing for bundling dynamic assets

--- 1-build/300x250/build.js
+++ 1-build/300x250/build.js
@@ -4,6 +4,9 @@ import * as Preflight from '@common/js/Preflight.js'
 
 import Control from './components/Control'
 
+// if rendering dps, creative server will add dps asset imports here 
+import './dps-assets.js'
+
 // called from index.html onImpression()
 const launch = async (binaryAssets) => {
 	console.log('Bundle.launch()')
