2f773646489bdf01cb8f39f28ecde53aba70b170
Greg Connell <gconnell@160over90.com>
2021-06-25 14:27:39 -0700

    leave stub for runtime image requests

--- 1-build/common/js/AdData.js
+++ 1-build/common/js/AdData.js
@@ -1,3 +1,22 @@
+import { ImageManager } from '@ff0000-ad-tech/ad-assets'
+
+/**
+ * TODO:
+ *	Dynamic HTML:
+ *		a. DPS needs to publish feeds/assets to a CDN
+ */
+
+/**
+	EXTRACT JSON DATA
+	Prepare dynamic data here.
+ */
+export const requestDynamicImages = async () => {
+	ImageManager.addImageRequest({ src: '/path/to/asset', imageId: 'my-dynamic-image' })
+}
+
+/**
+	DEFINE and EXPORT global Ad-Data
+ */
 export const copy = [
 	'WHY PAY MORE\nFOR ELECTRICITY?',
 	'MAKE THE MOST OF YOUR\nENERGY AND MONEY',
