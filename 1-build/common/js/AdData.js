91ccdc6a2a82b172de19f71c15c7b4498de258e7
Greg Connell <gconnell@160over90.com>
2021-06-25 14:23:52 -0700

    updating TODO

--- 1-build/common/js/AdData.js
+++ 1-build/common/js/AdData.js
@@ -3,10 +3,7 @@ import { DpsManager } from '@ff0000-ad-tech/ad-dps'
 /**
  * TODO:
  *	Dynamic HTML:
- *		a. 3-traffic: DPS will need to publish assets
- *	Rendered HTML (3-traffic, using cs-plugin):
- *		a. cs-plugin will download & add imports for any source-refs found in row being rendered
- *		b. cs-plugin will rewrite dpsConfig.data columns with `fba-bundled`
+ *		a. DPS needs to publish feeds/assets to a CDN
  */
 
 /**
