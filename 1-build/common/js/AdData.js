678a573e94cc65454a7739a05d7cf4940156d3b6
Greg Connell <gconnell@160over90.com>
2021-06-25 14:21:08 -0700

    simplify AdData, per ES6 exports

--- 1-build/common/js/AdData.js
+++ 1-build/common/js/AdData.js
@@ -1,29 +1,9 @@
-import { ImageManager } from '@ff0000-ad-tech/ad-control'
-
-export default function AdData() {
-	var self = this
-
-	/**
-		EXTRACT JSON DATA
-		Prepare dynamic data here.
-	 */
-
-	/**
-		DYNAMIC IMAGES
-		Dynamically loaded images need to be in their own directory, like "dynamic_images/".
-
-		Then, you need to add your dynamic image-paths to the load-queue, so that when
-		the secondary preload happens, these assets will get loaded. For example:
-
-		self.theImageName = ImageManager.addToLoad(adParams.imagesPath + 'sample.jpg');
-	 */
-	self.copy = ['WHY PAY MORE\nFOR ELECTRICITY?', 'MAKE THE MOST OF YOUR\nENERGY AND MONEY', 'CHOOSE SMART SAVINGS\nWITH DISCOUNT POWER.']
-	self.fonts = {
-		primary: 'template_font',
-	}
-
-	self.colors = {}
-
-	// Store svg markup for use in all UISvg instances, reduces duplicate code across builds.  See UISvg.
-	self.svg = {}
+export const copy = [
+	'WHY PAY MORE\nFOR ELECTRICITY?',
+	'MAKE THE MOST OF YOUR\nENERGY AND MONEY',
+	'CHOOSE SMART SAVINGS\nWITH DISCOUNT POWER.'
+]
+export const fonts = {
+	primary: 'template_font'
 }
+export const colors = {}
