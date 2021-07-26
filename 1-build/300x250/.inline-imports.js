bba43606a035d3f97ef23e4c2e9ed9e7d9552b22
Greg Connell <gconnell@160over90.com>
2021-07-26 16:31:59 -0700

    syncs index.html to no-fba-payload release

--- 1-build/300x250/.inline-imports.js
+++ 1-build/300x250/.inline-imports.js
@@ -1,3 +1,5 @@
 import { InlineSrc } from '@ff0000-ad-tech/ad-assets'
 window.InlineSrc = InlineSrc
 
+import preloaderBlack from './images/preloader_black.gif?inline=true'
+InlineSrc.add('./images/preloader_black.gif', preloaderBlack)
--- 1-build/common/js/Preflight.js
+++ 1-build/common/js/Preflight.js
@@ -7,11 +7,11 @@ import { ImageManager } from '@ff0000-ad-tech/ad-assets'
  * Resources that are shared by all the sizes should be loaded here.
  *
  */
-export const init = async (binaryAssets) => {
+export const init = async (assets) => {
 	// instantiate global ad-data
 	window.adData = AdData
 	// add binary payload
-	addFbaImages(binaryAssets)
+	addImageAssets([...assets.images, ...assets.binaries])
 	// add preloader images to build
 	addPreloadedImages()
 	// author adds necessary requests to queue
@@ -20,7 +20,7 @@ export const init = async (binaryAssets) => {
 	await loadDynamicImages()
 }
 
-const addFbaImages = async (binaryAssets) => {
+const addImageAssets = async (binaryAssets) => {
 	console.log('Preflight.addFbaImages()')
 	ImageManager.add(binaryAssets)
 }
