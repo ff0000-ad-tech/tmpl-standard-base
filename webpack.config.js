28a5c467c8e8fbd8ec715a6930149359c79ddc78
Greg Connell <gconnell@160over90.com>
2021-11-19 16:23:07 -0800

    roughly working wp5 compile

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,6 @@
  */
 const wp = require('./node_modules/@ff0000-ad-tech/wp-deploy-manager/webpack-config.js')
 
-module.exports = (config) => {
-	return wp.execute(config)
+module.exports = async (env) => {
+	return await wp.execute(env)
 }
