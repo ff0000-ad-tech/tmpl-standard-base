596dc7579c624a1b35105c8671b645231e813da9
Joe Capra <jcapra@160over90.com>
2021-11-24 10:03:09 -0800

    Glitch adjustments. RC2

--- 1-build/1000x1000/components/TwitchGlitch/index.jsx
+++ 1-build/1000x1000/components/TwitchGlitch/index.jsx
@@ -2,24 +2,23 @@ import { h, render, Component, createRef } from 'preact'
 import { ImageManager } from '@ff0000-ad-tech/ad-assets'
 
 import './styles.scss'
-
+import '@size/images/ref.png'
 class TwitchGlitch extends Component {
 	constructor(props) {
 		super(props)
 
 		this.glitch = (
-			<svg viewBox="0 0 2400 2800" class="twitchglitch__glitch">
+			<svg viewBox="0 0 2005 2542" class="twitchglitch__glitch">
 				<g>
-					<polygon class="twitchglitch__glitch-bg" points="2200,1300 1800,1700 1400,1700 1050,2050 1050,1700 600,1700 600,200 2200,200 	" />
+					<polygon class="twitchglitch__glitch-bg" points="1805,1300 1405,1700 1005,1700 655,2050 655,1700 205,1700 205,200 1805,200 	" />
 					<g>
 						<g id="Layer_1-2">
 							<path
 								class="twitchglitch__glitch-icon"
-								d="M500,0L0,500v1800h600v500l500-500h400l900-900V0H500z M2200,1300l-400,400h-400l-350,350v-350H600V200h1600
-				   V1300z"
+								d="M0,1905h451v637l639-637h439l476-505V0H0 M1805,1300l-400,400h-400l-350,350v-350H205V200h1600V1300z"
 							/>
-							<rect x="1700" y="550" class="twitchglitch__glitch-icon" width="200" height="600" />
-							<rect x="1150" y="550" class="twitchglitch__glitch-icon" width="200" height="600" />
+							<rect x="1305" y="550" class="twitchglitch__glitch-icon" width="200" height="600" />
+							<rect x="755" y="550" class="twitchglitch__glitch-icon" width="200" height="600" />
 						</g>
 					</g>
 				</g>
@@ -29,11 +28,12 @@ class TwitchGlitch extends Component {
 	componentDidMount() {
 		// this.start()
 		let bg_left = 'linear-gradient(90deg, rgba(145, 70, 255, 0) 0%, rgba(145, 70, 255, 1) 100%, rgba(145, 70, 255, 0) 100%)'
-		let bg_mid = 'linear-gradient(90deg, rgba(145, 70, 255, 0) 10%, rgba(145, 70, 255, 0.5) 100%, rgba(145, 70, 255, 0) 100%)'
+		let bg_mid = 'linear-gradient(90deg, rgba(145, 70, 255, 0) 10%, rgba(145, 70, 255, 1) 100%, rgba(145, 70, 255, 0) 100%)'
 		let bg_bottom = 'linear-gradient(0deg, rgba(145, 70, 255, 0) 0%, rgba(145, 70, 255, 1) 100%, rgba(145, 70, 255, 0) 100%)'
-		gsap.to('.twitchglitch__left', { duration: 2, backgroundImage: bg_left })
-		gsap.to('.twitchglitch__mid', { duration: 2, backgroundImage: bg_mid })
-		gsap.to('.twitchglitch__bottom', { duration: 2, backgroundImage: bg_bottom })
+		gsap.to('.twitchglitch__left', { duration: 0.3, backgroundImage: bg_left, ease: 'sine.out' })
+		gsap.to('.twitchglitch__mid', { duration: 0.3, backgroundImage: bg_mid, ease: 'sine.out' })
+		gsap.to('.twitchglitch__bottom', { duration: 0.3, backgroundImage: bg_bottom, ease: 'sine.out' })
+		gsap.from('.twitchglitch__glitch', { duration: 0.305, left: -5, top: 40, ease: 'sine.out' })
 	}
 
 	// Only used in demo for restarting animation. You can delete if you want
@@ -46,9 +46,12 @@ class TwitchGlitch extends Component {
 		const { debug } = this.props
 		return (
 			<div className="twitchglitch">
-				<div className="twitchglitch__left"></div>
-				<div className="twitchglitch__bottom"></div>
-				<div className="twitchglitch__mid"></div>
+				{/*<img src={ImageManager.get('ref').src} className="twitchglitch__ref" />*/}
+				<div className="twitchglitch__extrusion-container">
+					<div className="twitchglitch__left"></div>
+					<div className="twitchglitch__bottom"></div>
+					<div className="twitchglitch__mid"></div>
+				</div>
 				{this.glitch}
 			</div>
 		)
