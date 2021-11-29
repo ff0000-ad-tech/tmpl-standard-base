9e1281418a8fa46763ab7fcb973d9d91ca49fc9b
Joe Capra <jcapra@160over90.com>
2021-11-28 16:48:23 -0800

    Refactor streamerbug to all refs

--- 1-build/1000x1000/components/TwitchStreamerBug/index.jsx
+++ 1-build/1000x1000/components/TwitchStreamerBug/index.jsx
@@ -9,7 +9,7 @@ class TwitchStreamerBug extends Component {
 		super(props)
 
 		this.glitch = (
-			<svg viewBox="0 0 2400 2800" class="streamerbug__glitch">
+			<svg viewBox="0 0 2400 2800" class="streamerbug__glitch" ref={(el) => (this.glitchRef = el)}>
 				<g>
 					<polygon class="streamerbug__glitch-bg" points="2200,1300 1800,1700 1400,1700 1050,2050 1050,1700 600,1700 600,200 2200,200 	" />
 					<g>
@@ -43,18 +43,14 @@ class TwitchStreamerBug extends Component {
 	}
 
 	start() {
-		const highlightWidth = parseInt(getComputedStyle(this.highlight).getPropertyValue('width'))
+		const { bgRef, streamernameRef, highlightRef, glitchRef } = this
+		const highlightWidth = parseInt(getComputedStyle(highlightRef).getPropertyValue('width'))
 		this.tl = gsap.timeline()
-		this.tl.fromTo('.streamerbug__bg', { scaleX: 0, scaleY: 0 }, { scaleX: 0.25, scaleY: 1, duration: 0.3, ease: 'none' })
-		this.tl.to('.streamerbug__bg', { duration: 0.2, scaleX: 1 }, '-=.05')
-		this.tl.from('.streamerbug__glitch', { duration: 0.35, y: 30, ease: 'expo.out' }, '-=.2')
-		this.tl.from('.streamerbug__streamername', { duration: 0.35, y: 30, ease: 'expo.out' }, '-=.1')
-		this.tl.fromTo(
-			'.streamerbug__highlight',
-			{ x: -highlightWidth - 2 },
-			{ duration: 0.6, x: highlightWidth + 2, ease: 'sine.inOut' },
-			'-=.35'
-		)
+		this.tl.fromTo(bgRef, { scaleX: 0, scaleY: 0 }, { scaleX: 0.25, scaleY: 1, duration: 0.3, ease: 'none' })
+		this.tl.to(bgRef, { duration: 0.2, scaleX: 1 }, '-=.05')
+		this.tl.from(glitchRef, { duration: 0.35, y: 30, ease: 'expo.out' }, '-=.2')
+		this.tl.from(streamernameRef, { duration: 0.35, y: 30, ease: 'expo.out' }, '-=.1')
+		this.tl.fromTo(highlightRef, { x: -highlightWidth - 2 }, { duration: 0.6, x: highlightWidth + 2, ease: 'sine.inOut' }, '-=.35')
 		this.tl.play()
 
 		// Only used to demonstrate animate out
@@ -64,24 +60,28 @@ class TwitchStreamerBug extends Component {
 
 		// Original animation without timeline
 		// -----------------------------------------
-		// gsap.fromTo('.streamerbug__bg', { scaleX: 0, scaleY: 0 }, { scaleX: 0.25, scaleY: 1, duration: 0.3, ease: 'none' })
-		// gsap.to('.streamerbug__bg', { delay: 0.3, duration: 0.2, scaleX: 1 })
-		// gsap.from('.streamerbug__glitch', { delay: 0.2, duration: 0.3, y: 30, ease: 'expo.out' })
-		// gsap.from('.streamerbug__streamername', { delay: 0.3, duration: 0.3, y: 30, ease: 'expo.out' })
-		// gsap.fromTo('.streamerbug__highlight', { x: -highlightWidth }, { delay: 0.2, duration: 0.6, x: highlightWidth, ease: 'sine.inOut' })
+		// gsap.fromTo(bgRef, { scaleX: 0, scaleY: 0 }, { scaleX: 0.25, scaleY: 1, duration: 0.3, ease: 'none' })
+		// gsap.to(bgRef, { delay: 0.3, duration: 0.2, scaleX: 1 })
+		// gsap.from(glitchRef, { delay: 0.2, duration: 0.3, y: 30, ease: 'expo.out' })
+		// gsap.from(streamernameRef, { delay: 0.3, duration: 0.3, y: 30, ease: 'expo.out' })
+		// gsap.fromTo(highlightRef, { x: -highlightWidth }, { delay: 0.2, duration: 0.6, x: highlightWidth, ease: 'sine.inOut' })
 	}
 	animateOut() {
 		this.tl.reverse()
 	}
 	render() {
 		const { name } = this.props
+		const { streamerBugRef, bgRef, streamernameRef, highlightRef } = this
+
 		return (
-			<div className="streamerbug" ref={(el) => (this.streamerBug = el)}>
+			<div className="streamerbug" ref={(el) => (this.streamerBugRef = el)}>
 				<div className="streamerbug__container">
-					<div className="streamerbug__bg" />
+					<div className="streamerbug__bg" ref={(el) => (this.bgRef = el)} />
 					{this.glitch}
-					<div className="streamerbug__streamername">/{name}</div>
-					<div className="streamerbug__highlight" ref={(el) => (this.highlight = el)} />
+					<div className="streamerbug__streamername" ref={(el) => (this.streamernameRef = el)}>
+						/{name}
+					</div>
+					<div className="streamerbug__highlight" ref={(el) => (this.highlightRef = el)} />
 				</div>
 			</div>
 		)
